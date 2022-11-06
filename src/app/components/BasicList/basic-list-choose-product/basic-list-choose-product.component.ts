c37f1c79045b83062692a6ad44953c8faa778b88
shira farkash <shirafarkash8@gmail.com>
2022-11-06 14:42:15 +0200

    editable Basic List name

--- src/app/components/BasicList/basic-list-choose-product/basic-list-choose-product.component.ts
+++ src/app/components/BasicList/basic-list-choose-product/basic-list-choose-product.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from 'src/app/shared/models/product.models';
 import { ProductService } from 'src/app/shared/services/product.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-list-choose-product',
@@ -10,9 +11,9 @@ import { ProductService } from 'src/app/shared/services/product.service';
 })
 export class BasicListChooseProductComponent implements OnInit {
   allMailProduct:Array<Product>=new Array<Product>;
-  menuList=[true,false, false,false,false ]
-
-
+  menuList=[false,false, false,true,false ]
+  editable:boolean=false
+  listName:string="Basic List"
 
   constructor(private productService: ProductService, private router:Router,private ElByClassName: ElementRef) { }
  
@@ -24,22 +25,22 @@ export class BasicListChooseProductComponent implements OnInit {
     
   }
   ngAfterViewInit(){
-   
-
-
   }
+
   setActiveLink(n:number){
 
     for (let index = 0; index < this.menuList.length; index++) {
        this.menuList[index]=false;
     }
     this.menuList[n]=true
-
      console.log(this.menuList)
-
   }
 
+  editableClick(s:string){
+    this.editable=!this.editable
+    this.listName=s
 
+  }
 
   sortByName(){
     console.log("hello")
@@ -47,5 +48,7 @@ export class BasicListChooseProductComponent implements OnInit {
     console.log(this.allMailProduct[0].productName)
     console.log(this.allMailProduct)
   }
+
+
 
 }
