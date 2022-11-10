b0f57004a9a04a5a8a87e8001ddbb79bc883fb47
Jana Rappaport <janarappaport1@gmail.com>
2022-11-10 12:27:14 -0300

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BasicListChooseProductComponent } from './components/BasicList/basic-list-choose-product/basic-list-choose-product.component';
 import { IonicModule } from '@ionic/angular';
 import { MyAccountComponent } from './components/my-account/my-account.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +27,7 @@ import { MyAccountComponent } from './components/my-account/my-account.component
     ForgotPasswordComponent,
     BasicListChooseProductComponent,
     MyAccountComponent,
+    
     
   ],
   imports: [
