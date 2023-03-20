f95a31483a6e92a09b21bad792dd1626fe9ac107
JanaRappaport <janarappaport1@gmail.com>
2023-03-20 17:59:50 +0200

    cancel Button

--- src/app/components/Shopping/shopping-cart/shopping-cart.component.ts
+++ src/app/components/Shopping/shopping-cart/shopping-cart.component.ts
@@ -75,6 +75,9 @@ export class ShoppingCartComponent implements OnInit {
     })
 
   }
+  cancelCart(){
+    this.router.navigate(["/myCart"]);
+  }
   optionClicked() {
     switch (this.chosenOption) {
       case "name": {
